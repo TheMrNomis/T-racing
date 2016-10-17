3adfdb39480b689401d9b1ecec0454de69972ff2
Amaury Louarn <git@homnomnom.fr>
2016-10-17 18:13:39 +0200

    ajout d'une classe de spline vide
    à remplir plus tard, potentiellement avec des segments dans un premier
    temps

---
+++ Assets/scripts/Spline.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spline {
+    /*
+     * TODO:
+     * - 4 points
+     * - get une position en fonction de t (0 < t < 1)
+     * - get la distance entre un point et la courbe
+     */
+}
