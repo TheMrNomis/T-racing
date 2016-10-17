a95b57aebf830e351f4d4d33280e018e725dbd27
Evyle <les_sims_34@hotmail.fr>
2016-10-17 18:07:53 +0200

    add scene AC + script trajectory

---
+++ Assets/Scripts/Trajectory.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trajectory : MonoBehaviour {
+
+    public Vector3[] points;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
