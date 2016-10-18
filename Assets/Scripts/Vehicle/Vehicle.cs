fe9c2441f41eeab50fe5f576fa3994241e6e69a4
Karkariy <helali44@hotmail.fr>
2016-10-18 21:57:15 +0200

    adding vehicle class

--- Assets/Scripts/Vehicle/Vehicle.cs
+++ Assets/Scripts/Vehicle/Vehicle.cs
@@ -22,14 +22,22 @@ public class Vehicle : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         m_vRigidBody = GetComponent<Rigidbody>();
-
+        m_vRigidBody.velocity = new Vector3(1.0f, 0, 0);
     }
 
     // Update is called once per frame
     void Update () {
-        m_vRigidBody.velocity += m_acceleration;
-        Debug.Log("Velocity : " + m_vRigidBody.velocity.ToString());
 
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+             m_acceleration += new Vector3(-.1f,0,0);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            m_acceleration += new Vector3(.1f, 0, 0);
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            m_acceleration += new Vector3(0, 0, -.1f);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            m_acceleration += new Vector3(0, 0, 0.1f);
+
+        m_vRigidBody.velocity += m_acceleration;
     }
 
     public Vector3 GetVelocity() { return m_vRigidBody.velocity; }
