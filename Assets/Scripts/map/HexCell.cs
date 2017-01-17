ec422a6cd5a4dd99e91017b62acc38088b3ec3d2
clem <clem.delambily@gmail.com>
2017-01-17 16:55:13 +0100

    up map grande + route droite

---
+++ Assets/Scripts/map/HexCell.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour {
+
+	public HexCoordinates coordinates;
+
+    public RectTransform uiRect;
+
+    public HexGridChunk chunk;
+
+    public Color color;
+
+	[SerializeField]
+	HexCell[] neighbors;
+
+    [SerializeField]
+    bool[] roads;
+
+    public bool HasRoadThroughEdge(HexDirection direction){
+        return roads[(int)direction];
+    }
+
+    public bool HasRoads{
+        get{
+            for (int i = 0; i < roads.Length; i++){
+                if (roads[i]){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public void AddRoad(HexDirection direction){
+        if (!roads[(int)direction]){
+            SetRoad((int)direction, true);
+        }
+    }
+
+    public void RemoveRoads(){
+        for (int i = 0; i < neighbors.Length; i++){
+            if (roads[i]){
+                SetRoad(i, false);
+            }
+        }
+    }
+
+    void SetRoad(int index, bool state){
+        roads[index] = state;
+        neighbors[index].roads[(int)((HexDirection)index).Opposite()] = state;
+        neighbors[index].RefreshSelfOnly();
+        RefreshSelfOnly();
+    }
+
+    public HexCell GetNeighbor (HexDirection direction) {
+		return neighbors[(int)direction];
+	}
+
+	public void SetNeighbor (HexDirection direction, HexCell cell) {
+		neighbors[(int)direction] = cell;
+		cell.neighbors[(int)direction.Opposite()] = this;
+	}
+
+    void RefreshSelfOnly()
+    {
+        chunk.Refresh();
+    }
+
+    public Color Color {
+        get{ return color;
+        } set {
+            if (color == value) { return; }
+            color = value;
+            Refresh();
+        }
+    }
+    
+    void Refresh() {
+        if (chunk) {
+            chunk.Refresh();
+            for (int i = 0; i < neighbors.Length; i++) {
+                HexCell neighbor = neighbors[i];
+                if (neighbor != null && neighbor.chunk != chunk) {
+                    neighbor.chunk.Refresh();
+                }
+            }
+        }
+    }
+
+   
+}
---
+++ Assets/Scripts/map/HexMapEditor.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexMapEditor : MonoBehaviour {
+
+    enum OptionalToggle{
+        Ignore, Yes, No
+    }
+
+    bool applyColor;
+    public Color[] colors;
+	public HexGrid hexGrid;
+	private Color activeColor;
+    OptionalToggle roadMode;
+    bool isDrag;
+    HexDirection dragDirection;
+    HexCell previousCell;
+
+    void Awake () {
+		SelectColor(0);
+	}
+
+	void Update () {
+		if ( Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject() ) {
+			HandleInput();
+        }else{
+            previousCell = null;
+        }
+    }
+
+	void HandleInput () {
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(inputRay, out hit)) {
+            HexCell currentCell = hexGrid.GetCell(hit.point);
+            if (previousCell && previousCell != currentCell){
+                ValidateDrag(currentCell);
+            } else {
+                isDrag = false;
+            }
+            EditCell(hexGrid.GetCell(hit.point));
+            previousCell = currentCell;
+        }  else {
+            previousCell = null;
+        }
+    }
+
+
+    void ValidateDrag(HexCell currentCell){
+        for ( dragDirection = HexDirection.NE; dragDirection <= HexDirection.NW; dragDirection++ ) { 
+            if (previousCell.GetNeighbor(dragDirection) == currentCell) {
+                isDrag = true;
+                return;
+            }
+        }
+        isDrag = false;
+    }
+
+    void EditCell(HexCell cell){
+        if (cell)
+        {
+            if (applyColor){
+                cell.Color = activeColor;
+            }
+            if (roadMode == OptionalToggle.No) {
+                cell.RemoveRoads();
+            }
+            if (isDrag){
+                HexCell otherCell = cell.GetNeighbor(dragDirection.Opposite());
+                if (otherCell){
+                    if (roadMode == OptionalToggle.Yes) {
+                        otherCell.AddRoad(dragDirection);
+                    }
+                }
+            }
+        }
+    }
+
+    public void SelectColor(int index) {
+        applyColor = index >= 0;
+        if (applyColor) {
+            activeColor = colors[index];
+        }
+    }
+
+    public void SetRoadMode(int mode){
+        roadMode = (OptionalToggle)mode;
+    }
+}
